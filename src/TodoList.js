370f5eed1c9c404672b776b94dc65876755bbe91
OCGZszvMae <gloria.jin1222@gmail.com>
2021-01-13 18:01:45 -0500

    React-jianshu-3

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,16 +1,71 @@
 import React, { Component, Fragment } from 'react';
 
+import TodoItem from './TodoItem';
+import './style.css';
+
 class TodoList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: '',
+            list: []
+        };
+        this.handleInputChange=this.handleInputChange.bind(this);
+        this.handleBtnClick=this.handleBtnClick.bind(this);
+        this.handleItemDelete=this.handleItemDelete.bind(this);
+    }
+
     render() {
         return (
             <Fragment>
-                <div><input /><buttom>提交</buttom></div>
+                <div>
+                    <label htmlFor="insertArea">输入内容</label>
+                    <input 
+                        id="insertArea"
+                        className='input'
+                        value={this.state.inputValue}
+                        onChange={this.handleInputChange}/>
+                    <button
+                        onClick={this.handleBtnClick}>提交</button>
+                </div>
                 <ul>
-                    <li>English</li>
-                    <li>React</li>
+                    { this.getTodoItem() }
                 </ul>
             </Fragment>
         )
+    }
+
+    getTodoItem() {
+        return this.state.list.map((item, index)=>{
+            return <TodoItem 
+                        key={index}
+                        item={item}
+                        index={index}
+                        deleteItem={this.handleItemDelete}/>
+        })
+    }
+
+    handleInputChange(event){
+        const value = event.target.value
+        this.setState(()=>({
+            inputValue: value
+        }));
+    }
+
+    handleBtnClick(){
+        this.setState((prevState)=>({
+            list: [...prevState.list, prevState.inputValue],
+            inputValue: ''
+        }));
+    }
+
+    handleItemDelete(index){
+        this.setState((prevState)=>{
+            const list = [...prevState.list];
+            list.splice(index,1);
+            return {list}
+        })
     }
 }
 
